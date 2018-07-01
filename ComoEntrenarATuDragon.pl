c6870f26a0081558d3d522f1e66cca10fee378c1
Mariano Díaz <37786555+mdiazpereyra@users.noreply.github.com>
2018-07-01 03:38:31 -0300

    Update ComoEntrenarATuDragon.pl

--- ComoEntrenarATuDragon.pl
+++ ComoEntrenarATuDragon.pl
@@ -66,23 +66,83 @@ dragon(gronckle).
 dragon(chimuelo).
 dragon(salvajiBestia).
 
-obedeceA(chimuelo,hipo).
-obedeceA(gronckle,brutilda).
-obedeceA(gronckle,brutacio).
-obedeceA(nadderMortifero,astrid).
-obedeceA(chimuelo,salvajiBestia).
+obedeceA(chimuelo, hipo).
+obedeceA(gronckle, brutilda).
+obedeceA(gronckle, brutacio).
+obedeceA(nadderMortifero, astrid).
+obedeceA(chimuelo, salvajiBestia).
 
 lugar(islaDeBerk).
 lugar(islaDragon).
 
 viveEn(islaDragon, dragoManoDura).
-viveEn(islaDeBerk,hipo).
-viveEn(islaDeBerk,gobber).
-viveEn(islaDeBerk,astrid).
-viveEn(islaDeBerk,brutilda).
-viveEn(islaDeBerk,brutacio).
+viveEn(islaDeBerk, hipo).
+viveEn(islaDeBerk, gobber).
+viveEn(islaDeBerk, astrid).
+viveEn(islaDeBerk, brutilda).
+viveEn(islaDeBerk, brutacio).
 
 habilidad(chimuelo, explosion(grande, 5)).
 habilidad(salvajiBestia, hielo(-10)).
 habilidad(gronckle, fuego(300)).
 habilidad(nadderMortifero, fuego(500)).
+
+%salvajibestia domina a todos los dragones menos a chimuelo.
+%chimuelo domina a todos los dragones.
+
+%----------PUNTO 1----------
+
+puedeEntrenar(hipo, Dragon):-
+	dragon(Dragon).
+puedeEntrenar(brutilda, gronckle).
+puedeEntrenar(brutacio, gronckle).
+puedeEntrenar(astrid, Dragon):-
+	dragon(Dragon),
+	not(puedeEntrenar(brutilda, Dragon)),
+	not(puedeEntrenar(brutacio, Dragon)).
+puedeEntrenar(dragoManoDura, salvajiBestia).
+
+%----------PUNTO 2----------
+	
+esMonopolioDeDragones(Lugar):-
+	lugar(Lugar),
+	forall((dragon(Dragon),obedeceA(Dragon, entrenador(Entrenador))), viveEn(Lugar, Entrenador)).
+
+%----------PUNTO 3----------
+
+%-a-
+esDominadoPor(Dragon, chimuelo):-
+	dragon(Dragon).
+esDominadoPor(Dragon, salvajiBestia):-
+	dragon(Dragon),
+	Dragon \= chimuelo.
+	
+esDragonAlfa(DragonAlfa):-
+	dragon(DragonAlfa),
+	forall(dragon(Dragon), esDominadoPor(Dragon, DragonAlfa)).
+	
+%-b-
+%Ver base de conocimiento.
+	
+%----------PUNTO 4----------	
+
+esDragonSalvaje(Dragon):-
+	not(obedeceA(Dragon, _)).
+
+%----------PUNTO 5----------
+
+esDragonSumiso(Dragon):-
+	obedeceA(Dragon, Dominador),
+	esDragonAlfa(Dominador).
+	
+%----------PUNTO 6----------
+
+esDragonPeligroso(Dragon):-
+	habilidad(Dragon, fuego(MetrosDeLlamarada)),
+	MetrosDeLlamarada > 400.
+esDragonPeligroso(Dragon):-
+	habilidad(Dragon, explosion(_, NumeroDeExplosiones)),
+	NumeroDeExplosiones > 3.
+esDragonPeligroso(Dragon):-
+	habilidad(Dragon, hielo(Grados)),
+	Grados < 0.
